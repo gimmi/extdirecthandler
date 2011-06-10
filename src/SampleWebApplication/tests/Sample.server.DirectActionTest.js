d942b56b0d38998e48d6db348ad9983101a1fac5
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2011-06-10 09:14:23 +0200

    Added JS test infrastructure and a test

---
+++ src/SampleWebApplication/tests/Sample.server.DirectActionTest.js
@@ -0,0 +1,25 @@
+﻿/*global describe, beforeEach, expect, it */
+
+describe("Sample.server.DirectAction", function () {
+	var target;
+
+	beforeEach(function () {
+		target = Sample.server.DirectAction;
+	});
+
+	it('should echo value', function () {
+		var actual;
+		runs(function() {
+			target.echo('hello world', function (ret) {
+				this.done = true;
+				actual = ret;
+			}, this);
+		});
+		waitsFor(function () {
+			return this.done;
+		});
+		runs(function () {
+			expect(actual).toEqual('hello world');
+		});
+	});
+});
