b1daef63235912852707cefa6b5c44007af675b9
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2011-06-12 20:40:44 +0200

    Added support for "direct-tree.html" Ext.Direct example

--- src/ExtSamplesRunner/TestAction.cs
+++ src/ExtSamplesRunner/TestAction.cs
@@ -17,52 +17,61 @@ namespace ExtSamplesRunner
 
 		public IEnumerable<GetGridResult> GetGrid(GetGridParams par)
 		{
-			IEnumerable<GetGridResult> data = new[]{
-				new GetGridResult{ Name = "ABC Accounting", Turnover = 50000 },
-				new GetGridResult{ Name = "Ezy Video Rental", Turnover = 106300 },
-				new GetGridResult{ Name = "Greens Fruit Grocery", Turnover = 120000 },
-				new GetGridResult{ Name = "Icecream Express", Turnover = 73000 },
-				new GetGridResult{ Name = "Ripped Gym", Turnover = 88400 },
-				new GetGridResult{ Name = "Smith Auto Mechanic", Turnover = 222980 }
+			IEnumerable<GetGridResult> data = new[] {
+				new GetGridResult {Name = "ABC Accounting", Turnover = 50000},
+				new GetGridResult {Name = "Ezy Video Rental", Turnover = 106300},
+				new GetGridResult {Name = "Greens Fruit Grocery", Turnover = 120000},
+				new GetGridResult {Name = "Icecream Express", Turnover = 73000},
+				new GetGridResult {Name = "Ripped Gym", Turnover = 88400},
+				new GetGridResult {Name = "Smith Auto Mechanic", Turnover = 222980}
 			};
-			foreach(GetGridParams.SortInfo sortInfo in par.Sort)
+			foreach (GetGridParams.SortInfo sortInfo in par.Sort)
 			{
 				data = sortInfo.Sort(data);
 			}
 			return data;
 		}
 
+		public IEnumerable<GetTreeResult> GetTree(string id)
+		{
+			if (id == "root")
+			{
+				return new[] {1, 2, 3, 4, 5}.Select(n => new GetTreeResult {Id = n.ToString(), Text = "Node " + n, Leaf = false});
+			}
+			return new[] {1, 2, 3, 4, 5}.Select(n => new GetTreeResult {Id = id + n, Text = "Node " + id + "." + n, Leaf = true});
+		}
+
 		#region Nested type: GetGridParams
 
 		public class GetGridParams
 		{
-			public int Page;
-			public int Start;
 			public int Limit;
+			public int Page;
 			public SortInfo[] Sort;
+			public int Start;
 
 			#region Nested type: SortInfo
 
 			public class SortInfo
 			{
-				public string Property;
 				public string Direction;
+				public string Property;
 
 				public IEnumerable<GetGridResult> Sort(IEnumerable<GetGridResult> data)
 				{
-					if(Property == "name" && Direction == "ASC")
+					if (Property == "name" && Direction == "ASC")
 					{
 						data = data.OrderBy(x => x.Name);
 					}
-					else if(Property == "name" && Direction == "DESC")
+					else if (Property == "name" && Direction == "DESC")
 					{
 						data = data.OrderByDescending(x => x.Name);
 					}
-					else if(Property == "turnover" && Direction == "ASC")
+					else if (Property == "turnover" && Direction == "ASC")
 					{
 						data = data.OrderBy(x => x.Turnover);
 					}
-					else if(Property == "turnover" && Direction == "DESC")
+					else if (Property == "turnover" && Direction == "DESC")
 					{
 						data = data.OrderByDescending(x => x.Turnover);
 					}
@@ -81,6 +90,17 @@ namespace ExtSamplesRunner
 		{
 			public string Name;
 			public int Turnover;
+		}
+
+		#endregion
+
+		#region Nested type: GetTreeResult
+
+		public class GetTreeResult
+		{
+			public string Id;
+			public bool Leaf;
+			public string Text;
 		}
 
 		#endregion
