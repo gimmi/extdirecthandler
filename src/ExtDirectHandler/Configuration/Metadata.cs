b4ce1d681ecfe9b3f6809b2ee4f8c1aaf4ec2530
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2011-06-13 19:33:58 +0200

    Refactoring on Metadata class

--- src/ExtDirectHandler/Configuration/Metadata.cs
+++ src/ExtDirectHandler/Configuration/Metadata.cs
@@ -6,52 +6,65 @@ namespace ExtDirectHandler.Configuration
 {
 	public class Metadata
 	{
-		private class ActionMetadata
-		{
-			public Type Type;
-			public readonly IDictionary<string, MethodInfo> Methods = new Dictionary<string, MethodInfo>();
-		}
-
-		private static readonly IDictionary<string, ActionMetadata> _cache = new Dictionary<string, ActionMetadata>();
+		private static readonly IDictionary<string, ActionMetadata> Cache = new Dictionary<string, ActionMetadata>();
 
 		public virtual void AddAction(string actionName, Type type)
 		{
-			_cache.Add(actionName, new ActionMetadata{ Type = type });
+			Cache.Add(actionName, new ActionMetadata{ Type = type });
 		}
 
 		public virtual void AddMethod(string actionName, string methodName, MethodInfo methodInfo)
 		{
-			_cache[actionName].Methods.Add(methodName, methodInfo);
+			Cache[actionName].Methods.Add(methodName, new MethodMetadata{ MethodInfo = methodInfo });
 		}
 
 		public virtual Type GetActionType(string actionName)
 		{
-			return _cache[actionName].Type;
+			return Cache[actionName].Type;
 		}
 
 		public virtual IEnumerable<string> GetActionNames()
 		{
-			return _cache.Keys;
+			return Cache.Keys;
 		}
 
 		public virtual IEnumerable<string> GetMethodNames(string actionName)
 		{
-			return _cache[actionName].Methods.Keys;
+			return Cache[actionName].Methods.Keys;
 		}
 
 		public virtual MethodInfo GetMethodInfo(string actionName, string methodName)
 		{
-			return _cache[actionName].Methods[methodName];
+			return Cache[actionName].Methods[methodName].MethodInfo;
 		}
 
 		public virtual int GetNumberOfParameters(string actionName, string methodName)
 		{
-			return _cache[actionName].Methods[methodName].GetParameters().Length;
+			return Cache[actionName].Methods[methodName].MethodInfo.GetParameters().Length;
 		}
 
 		public virtual bool IsFormHandler(string actionName, string methodName)
 		{
 			return (actionName == "Profile" && methodName == "updateBasicInfo"); // TODO this is just to try Ext examples
 		}
+
+		#region Nested type: ActionMetadata
+
+		private class ActionMetadata
+		{
+			public readonly IDictionary<string, MethodMetadata> Methods = new Dictionary<string, MethodMetadata>();
+			public Type Type;
+		}
+
+		#endregion
+
+		#region Nested type: MethodMetadata
+
+		private class MethodMetadata
+		{
+			public MethodInfo MethodInfo;
+		}
+
+		#endregion
 	}
 }
