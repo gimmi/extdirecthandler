78db5d0d226e8876f9049594f514f6048cea0680
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2011-10-27 17:52:49 +0200

    Added LambdaConfigurator as a new way to configure ExtDirectHandler

---
+++ src/ExtDirectHandler.Tests/Configuration/LambdaConfiguratorTest.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using ExtDirectHandler.Configuration;
+using NUnit.Framework;
+using SharpTestsEx;
+
+namespace ExtDirectHandler.Tests.Configuration
+{
+	[TestFixture]
+	public class LambdaConfiguratorTest
+	{
+		private LambdaConfigurator _target;
+
+		[SetUp]
+		public void SetUp()
+		{
+			_target = new LambdaConfigurator();
+		}
+
+		[Test]
+		public void Should_register_methods_with_no_parameters_and_no_return_value()
+		{
+			_target.Register<TestClass>("Service.noParNoRet", x => x.NoParNoRet());
+			_target.GetMethodInfo("Service", "noParNoRet").Should().Be.SameInstanceAs(typeof(TestClass).GetMethod("NoParNoRet"));
+		}
+
+		[Test]
+		public void Should_register_methods_with_no_parameters_and_return_value()
+		{
+			_target.Register<TestClass>("Service.noParWithRet", x => x.NoParWithRet());
+			_target.GetMethodInfo("Service", "noParWithRet").Should().Be.SameInstanceAs(typeof(TestClass).GetMethod("NoParWithRet"));
+		}
+
+		[Test]
+		public void Should_register_methods_with_parameters_and_no_return_value()
+		{
+			_target.Register<TestClass>("Service.withParNoRet", x => x.WithParNoRet(default(int), default(string)));
+			_target.GetMethodInfo("Service", "withParNoRet").Should().Be.SameInstanceAs(typeof(TestClass).GetMethod("WithParNoRet"));
+		}
+
+		[Test]
+		public void Should_register_methods_with_parameters_and_return_value()
+		{
+			_target.Register<TestClass>("Service.withParWithRet", x => x.WithParWithRet(default(int), default(string)));
+			_target.GetMethodInfo("Service", "withParWithRet").Should().Be.SameInstanceAs(typeof(TestClass).GetMethod("WithParWithRet"));
+		}
+
+		[Test]
+		public void Should_throw_exception_when_method_not_registered()
+		{
+			Executing.This(() => _target.GetMethodInfo("Action", "NotRegistered")).Should().Throw<ArgumentException>().And.ValueOf.Message.Should().Be.EqualTo("Method 'Action.NotRegistered' not registered");
+		}
+
+		[Test]
+		public void Should_throw_exception_when_name_not_in_the_expected_format()
+		{
+			Executing.This(() => _target.Register<TestClass>("method", x => x.NoParNoRet())).Should().Throw<ArgumentException>().And.ValueOf.Message.Should().Be.EqualTo("Invalid name 'method'. Name must be in the form 'Action.method'");
+			Executing.This(() => _target.Register<TestClass>("Ns.Action.method", x => x.NoParNoRet())).Should().Throw<ArgumentException>().And.ValueOf.Message.Should().Be.EqualTo("Invalid name 'Ns.Action.method'. Name must be in the form 'Action.method'");
+		}
+
+		[Test]
+		public void Should_throw_exception_when_registering_the_same_method_name_multiple_times()
+		{
+			_target.Register<TestClass>("Action.method", x => x.NoParNoRet());
+			Executing.This(() => _target.Register<TestClass>("Action.method", x => x.NoParNoRet())).Should().Throw<ArgumentException>().And.ValueOf.Message.Should().Be.EqualTo("Method 'Action.method' already registered");
+		}
+
+		public class TestClass
+		{
+			public void NoParNoRet() {}
+
+			public int NoParWithRet()
+			{
+				return 0;
+			}
+
+			public void WithParNoRet(int p1, string p2) {}
+
+			public string WithParWithRet(int p1, string p2)
+			{
+				return null;
+			}
+		}
+	}
+}
---
+++ src/ExtDirectHandler/Configuration/LambdaConfigurator.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace ExtDirectHandler.Configuration
+{
+	public class LambdaConfigurator : IMetadata
+	{
+		private readonly IDictionary<string, IDictionary<string, MethodMetadata>> _cache = new Dictionary<string, IDictionary<string, MethodMetadata>>();
+		private string _namespace;
+
+		#region IMetadata Members
+
+		public string GetNamespace()
+		{
+			return _namespace;
+		}
+
+		public IEnumerable<string> GetActionNames()
+		{
+			return _cache.Keys;
+		}
+
+		public IEnumerable<string> GetMethodNames(string actionName)
+		{
+			return _cache[actionName].Keys;
+		}
+
+		public MethodInfo GetMethodInfo(string actionName, string methodName)
+		{
+			return GetMethodMetadata(actionName, methodName).MethodInfo;
+		}
+
+		public int GetNumberOfParameters(string actionName, string methodName)
+		{
+			return GetMethodMetadata(actionName, methodName).MethodInfo.GetParameters().Length;
+		}
+
+		public bool IsFormHandler(string actionName, string methodName)
+		{
+			return GetMethodMetadata(actionName, methodName).IsFormHandler;
+		}
+
+		public bool HasNamedArguments(string actionName, string methodName)
+		{
+			return GetMethodMetadata(actionName, methodName).HasNamedArguments;
+		}
+
+		public IEnumerable<string> GetArgumentNames(string actionName, string methodName)
+		{
+			return GetMethodMetadata(actionName, methodName).MethodInfo.GetParameters().Select(p => p.Name);
+		}
+
+		#endregion
+
+		public LambdaConfigurator Register(string name, Expression<Action> expression, bool namedArguments = false, bool formHandler = false)
+		{
+			return Register(name, (LambdaExpression)expression, namedArguments, formHandler);
+		}
+
+		public LambdaConfigurator Register<T>(string name, Expression<Action<T>> expression, bool namedArguments = false, bool formHandler = false)
+		{
+			return Register(name, (LambdaExpression)expression, namedArguments, formHandler);
+		}
+
+		public LambdaConfigurator Register<T, TResult>(string name, Expression<Func<T, TResult>> expression, bool namedArguments = false, bool formHandler = false)
+		{
+			return Register(name, (LambdaExpression)expression, namedArguments, formHandler);
+		}
+
+		public LambdaConfigurator Register(string name, LambdaExpression expression, bool namedArguments = false, bool formHandler = false)
+		{
+			var outermostExpression = expression.Body as MethodCallExpression;
+			if(outermostExpression == null)
+			{
+				throw new ArgumentException("Invalid Expression. Expression must consist of a Method call only.");
+			}
+			string[] nameParts = name.Split('.');
+			if(nameParts.Length != 2)
+			{
+				throw new ArgumentException(string.Format("Invalid name '{0}'. Name must be in the form 'Action.method'", name));
+			}
+			if(!_cache.ContainsKey(nameParts[0]))
+			{
+				_cache.Add(nameParts[0], new Dictionary<string, MethodMetadata>());
+			}
+			if (_cache[nameParts[0]].ContainsKey(nameParts[1]))
+			{
+				throw new ArgumentException(string.Format("Method '{0}.{1}' already registered", nameParts[0], nameParts[1]));
+			}
+			_cache[nameParts[0]].Add(nameParts[1], new MethodMetadata {
+				MethodInfo = outermostExpression.Method,
+				HasNamedArguments = namedArguments,
+				IsFormHandler = formHandler
+			});
+			return this;
+		}
+
+		public LambdaConfigurator SetNamespace(string ns)
+		{
+			_namespace = ns;
+			return this;
+		}
+
+		private MethodMetadata GetMethodMetadata(string actionName, string methodName)
+		{
+			try
+			{
+				return _cache[actionName][methodName];
+			}
+			catch (KeyNotFoundException e)
+			{
+				throw new ArgumentException(string.Format("Method '{0}.{1}' not registered", actionName, methodName));
+			}
+		}
+
+		#region Nested type: MethodMetadata
+
+		private class MethodMetadata
+		{
+			public bool HasNamedArguments;
+			public bool IsFormHandler;
+			public MethodInfo MethodInfo;
+		}
+
+		#endregion
+	}
+}
