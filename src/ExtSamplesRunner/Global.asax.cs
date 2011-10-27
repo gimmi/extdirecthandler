4435c3f1e1884d62bda2cd9251aefc9fd1324827
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2011-10-27 19:47:49 +0200

    Added lambda based configuration for sample projects

--- src/ExtSamplesRunner/Global.asax.cs
+++ src/ExtSamplesRunner/Global.asax.cs
@@ -9,9 +9,21 @@ namespace ExtSamplesRunner
 	{
 		protected void Application_Start(object sender, EventArgs e)
 		{
-			DirectHttpHandler.SetMetadata(new ReflectionConfigurator()
-			                                     	.RegisterType<TestAction>()
-			                                     	.RegisterType<Profile>());
+			//DirectHttpHandler.SetMetadata(new ReflectionConfigurator()
+			//                                        .RegisterType<TestAction>()
+			//                                        .RegisterType<Profile>());
+
+			var c = new LambdaConfigurator();
+			c.Register<TestAction>("TestAction.doEcho", x => x.DoEcho(default(string)));
+			c.Register<TestAction>("TestAction.multiply", x => x.Multiply(default(double)));
+			c.Register<TestAction>("TestAction.getGrid", x => x.GetGrid(default(TestAction.GetGridParams)));
+			c.Register<TestAction>("TestAction.getTree", x => x.GetTree(default(string)));
+			c.Register<TestAction>("TestAction.showDetails", x => x.ShowDetails(default(string), default(string), default(int)), namedArguments: true);
+			c.Register<Profile>("Profile.getBasicInfo", x => x.GetBasicInfo(default(int), default(string)));
+			c.Register<Profile>("Profile.getPhoneInfo", x => x.GetPhoneInfo(default(int)));
+			c.Register<Profile>("Profile.getLocationInfo", x => x.GetLocationInfo(default(int)));
+			c.Register<Profile>("Profile.updateBasicInfo", x => x.UpdateBasicInfo(default(string), default(string), default(string), default(string), default(string)), formHandler: true);
+			DirectHttpHandler.SetMetadata(c);
 		}
 
 		protected void Session_Start(object sender, EventArgs e) {}
--- src/SampleWebApplication/Global.asax.cs
+++ src/SampleWebApplication/Global.asax.cs
@@ -12,6 +12,19 @@ namespace SampleWebApplication
 			DirectHttpHandler.SetMetadata(new ReflectionConfigurator()
 			                              	.SetNamespace("Sample.server")
 			                              	.RegisterType<DirectAction>());
+			//DirectHttpHandler.SetMetadata(new LambdaConfigurator()
+			//                                .SetNamespace("Sample.server")
+			//                                .Register<DirectAction>("DirectAction.stringEcho", x => x.StringEcho(default(string)))
+			//                                .Register<DirectAction>("DirectAction.numberEcho", x => x.NumberEcho(default(double)))
+			//                                .Register<DirectAction>("DirectAction.boolEcho", x => x.BoolEcho(default(bool)))
+			//                                .Register<DirectAction>("DirectAction.arrayEcho", x => x.ArrayEcho(default(int[])))
+			//                                .Register<DirectAction>("DirectAction.objectEcho", x => x.ObjectEcho(default(DirectAction.ExampleClass)))
+			//                                .Register<DirectAction>("DirectAction.jObjectEcho", x => x.JObjectEcho(default(JObject)))
+			//                                .Register<DirectAction>("DirectAction.noParams", x => x.NoParams())
+			//                                .Register<DirectAction>("DirectAction.exception", x => x.Exception())
+			//                                .Register<DirectAction>("DirectAction.namedArguments", x => x.NamedArguments(default(string), default(double), default(bool)), namedArguments: true)
+			//                                .Register<DirectAction>("DirectAction.submitFile", x => x.SubmitFile(default(string), default(Stream)), formHandler: true)
+			//    );
 		}
 
 		protected void Session_Start(object sender, EventArgs e) {}
