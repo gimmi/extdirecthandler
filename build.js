81bfa5a7b418995d6f51aa4b458bdd90654223f7
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2012-10-14 16:43:00 +0200

    Updated automatic build to work with the current NuGet

--- build.js
+++ build.js
@@ -8,12 +8,13 @@ var sys = jsmake.Sys;
 var dotnet = new jsmake.dotnet.DotNetUtils();
 var javascript = new jsmake.javascript.JavascriptUtils();
 
-var version;
+var version, assemblyVersion;
 
 task('default', 'test');
 
 task('version', function () {
 	version = JSON.parse(fs.readFile('version.json'));
+	assemblyVersion = [ version.major, version.minor, version.build, 0 ].join('.');
 });
 
 task('dependencies', function () {
@@ -30,9 +31,9 @@ task('assemblyinfo', 'version', function () {
 		AssemblyTrademark: '',
 		AssemblyCompany: 'Gian Marco Gherardi',
 		AssemblyConfiguration: '', // Probably a good place to put Git SHA1 and build date
-		AssemblyVersion: [ version.major, version.minor, version.build, 0 ].join('.'),
-		AssemblyFileVersion: [ version.major, version.minor, version.build, 0 ].join('.'),
-		AssemblyInformationalVersion: [ version.major, version.minor, version.build, 0 ].join('.')
+		AssemblyVersion: assemblyVersion,
+		AssemblyFileVersion: assemblyVersion,
+		AssemblyInformationalVersion: assemblyVersion
 	});
 });
 
@@ -61,11 +62,11 @@ task('test', 'build', function () {
 
 task('release', 'test', function () {
 	fs.deletePath('build');
-	dotnet.deployToNuGet('src/ExtDirectHandler/ExtDirectHandler.csproj', 'build', true);
-/*
-	dotnet.runMSBuild('src/ExtDirectHandler.sln', [ 'Clean', 'ExtDirectHandler:Rebuild' ]);
-	fs.zipPath('build/bin', 'build/extdirecthandler-' + [ version.major, version.minor, version.build, version.revision ].join('.') + '.zip');
-*/
+	fs.createDirectory('build');
+
+	sys.run('tools/nuget/nuget.exe', 'pack', 'src\\ExtDirectHandler\\ExtDirectHandler.csproj', '-Build', '-OutputDirectory', 'build', '-Symbols');
+	sys.run('tools/nuget/nuget.exe', 'push', 'build\\ExtDirectHandler.' + assemblyVersion + '.nupkg');
+
 	version.build += 1;
 	fs.writeFile('version.json', JSON.stringify(version));
 });
